e0bf2dac6efac149fcdd3c8cfe3aaf89ecfa5f6d
dudital1 <dudital1@gmail.com>
2020-12-15 18:05:45 +0200

    Controller

--- controller.js
+++ controller.js
@@ -1,5 +1,13 @@
-<<<<<<< HEAD
-console.log("Saar test");
-=======
-console.log("hello from dudi");
->>>>>>> 8f203475dbe303feb9d2fbee1e6f901845910b6b
+const {cards} = require('');
+
+exports.cardController = {
+    getCards(req,res){
+        console.log('All restaurants data requested!');
+        res.json(cards);
+    },
+    getCards(req,res){
+        card = cards.find(c => c.id === parseInt(req.params.id));
+        console.log(`User with id ${req.params.id} requested!`);
+        res.send(card);
+    }
+};
